4209f3f6dc475f4737392f41a1b1b1e6620a2634
demosche <misan3760@gmail.com>
2025-10-29 21:51:19 +0500

    Task completed SumAndMean

---
+++ part1/SumAndMean.py
@@ -0,0 +1,7 @@
+fir = int(input("Number 1: "))
+sec = int(input("Number 2: "))
+thi = int(input("Number 3: "))
+four = int(input("Number 4: "))
+sum = fir + sec + thi + four
+mean = sum / 4
+print(f"The sum of the numbers is {sum} and the mean is {mean}")
