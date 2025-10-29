8727d66b49efaca8de0f961ac395b8460aca6e8c
demosche <misan3760@gmail.com>
2025-10-29 21:45:05 +0500

    Task completed Sum And Product

---
+++ part1/5sumAndProduct.py
@@ -0,0 +1,6 @@
+fir = int(input("Number 1: "))
+sec = int(input("Number 2: "))
+sum = fir + sec
+mult = fir * sec
+print(f"The sum of the numbers: {sum}")
+print(f"The product of the numbers: {mult}")
