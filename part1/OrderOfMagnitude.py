c0ca5b71ead1f358e94e2281633b4160a569642b
demosche <misan3760@gmail.com>
2025-11-03 18:22:49 +0500

    complete OrderOfMagnitude

---
+++ part1/OrderOfMagnitude.py
@@ -0,0 +1,8 @@
+number = int(input("Please type in a number: "))
+
+if number < 1000:
+    print("This number is smaller than 1000")
+if number < 100:
+    print("This number is smaller than 100")
+
+print("Thank you!")
