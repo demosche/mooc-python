b4f95edbc94a064cf139248308b4d34bbfd91bcd
demosche <misan3760@gmail.com>
2025-11-03 18:13:11 +0500

    Complete FoodExpenditure

---
+++ part1/FoodExpenditure.py
@@ -0,0 +1,10 @@
+days = int(input("How many times a week do you eat at the student cafeteria? "))
+lunch = float(input("The price of a typical student lunch? "))
+groceries = float(input("How much money do you spend on groceries in a week? "))
+
+weekly = days * lunch + groceries
+daily = weekly / 7
+
+print("Average food expenditure:")
+print(f"Daily: {daily} euros")
+print(f"Weekly: {weekly} euros")
