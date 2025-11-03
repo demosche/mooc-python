f3649fa4b7e44e05b122de627796276e8c41274d
demosche <misan3760@gmail.com>
2025-11-03 18:21:12 +0500

    Complete SoupOrNoSoup

---
+++ part1/SoupOrNoSoup.py
@@ -0,0 +1,8 @@
+name = input("Please tell me your name: ")
+
+if name != "Jerry":
+    port = int(input("How many portions of soup? "))
+    cost = port * 5.90
+    print("The total cost is", cost)
+
+print("Next please!")
