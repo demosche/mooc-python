d04b3a41a9f68654b9a5eb1d213dab880fc16ff5
demosche <misan3760@gmail.com>
2025-12-14 18:06:18 +0500

    Complete InputValidation

---
+++ part2/16.InputValidation.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+while True:
+    num = int(input("Please type in a number: "))
+
+    if num == 0:
+        print("Exiting...")
+        break
+    elif num < 0:
+        print("Invalid number")
+    else:
+        print(sqrt(num))
