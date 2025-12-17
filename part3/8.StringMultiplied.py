eac87a670881fa4bf18c79ef35bd0dc08806c96e
demosche <misan3760@gmail.com>
2025-12-17 20:40:37 +0500

    Fixed StringMultiplied

--- part3/8.StringMultiplied.py
+++ part3/8.StringMultiplied.py
@@ -1,10 +1,4 @@
-string = input("Please type in a string: ")
+text = input("Please type in a string: ")
 amount = int(input("Please type in an amount: "))
 
-result = ""
-counter = 0
-while counter < amount:
-    result += string
-    counter += 1
-
-print(result)
+print(text * amount)
