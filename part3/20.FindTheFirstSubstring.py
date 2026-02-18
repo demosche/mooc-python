901a4636092bebdb4a148accd0313086e59cc257
demosche <misan3760@gmail.com>
2026-02-18 18:35:13 +0500

    completed FindTheFirstSubstring

---
+++ part3/20.FindTheFirstSubstring.py
@@ -0,0 +1,6 @@
+word = input("Please type in a word: ")
+char = input("Please type in a character: ")
+
+index = word.find(char)
+if index != -1 and index <= len(word) - 3:
+    print(word[index:index+3])
