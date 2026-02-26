0688c0399027c16b41192e2da7678304663f79a5
demosche <misan3760@gmail.com>
2026-02-26 18:27:41 +0500

    completed SevenBrothers

---
+++ part3/28.SevenBrothers.py
@@ -0,0 +1,7 @@
+def seven_brothers():
+    bros = ["Aapo", "Eero", "Juhani", "Lauri", "Simeoni", "Timo", "Tuomas"]
+    bros.sort()
+    for bro in bros:
+        print(bro)
+if __name__ == "__main__":
+    seven_brothers()
