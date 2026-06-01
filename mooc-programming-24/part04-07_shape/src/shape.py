e83e8f9394c1ba46a0954bcb66dfb2ff7be39a55
demosche <misan3760@gmail.com>
2026-06-01 21:15:31 +0500

    complete shape

--- mooc-programming-24/part04-07_shape/src/shape.py
+++ mooc-programming-24/part04-07_shape/src/shape.py
@@ -1,4 +1,21 @@
-# Copy here code of line function from previous exercise and use it in your solution
-# You can test your function by calling it within the following block
+def line(length, char):
+    """Prints a line of the given character with the specified length"""
+    print(char * length)
+
+def triangle(size, char):
+    """Prints a triangle of given size using the specified character"""
+    for i in range(1, size + 1):
+        line(i, char)
+
+def rectangle(width, height, char):
+    """Prints a rectangle of given width and height using the specified character"""
+    for i in range(height):
+        line(width, char)
+
+def shape(triHei, triChar, recHei, recChar):
+    """Prints a triangle followed by a rectangle below it"""
+    triangle(triHei, triChar)
+    rectangle(triHei, recHei, recChar)
+
 if __name__ == "__main__":
     shape(5, "x", 2, "o")
