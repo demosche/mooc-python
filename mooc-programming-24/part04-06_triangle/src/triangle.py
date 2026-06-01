cba63c5fb92896d588fb65ec1778db6285138623
demosche <misan3760@gmail.com>
2026-06-01 21:13:03 +0500

    complete triangle

--- mooc-programming-24/part04-06_triangle/src/triangle.py
+++ mooc-programming-24/part04-06_triangle/src/triangle.py
@@ -1,9 +1,13 @@
 # Copy here code of line function from previous exercise
+def line(length, char):
+    """Prints a line of the given character with the specified length"""
+    print(char * length)
 
 def triangle(size):
-    # You should call function line here with proper parameters
-    line(4, "#")
+    for i in range(1, size + 1):
+        line(i, "#")
 
-# You can test your function by calling it within the following block
 if __name__ == "__main__":
     triangle(5)
+    print()
+    triangle(3)
