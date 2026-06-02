59b802ed9cebd81adaa601642b101dff72d3a97b
demosche <misan3760@gmail.com>
2026-06-02 18:28:32 +0500

    complete spruce

--- mooc-programming-24/part04-08_spruce/src/spruce.py
+++ mooc-programming-24/part04-08_spruce/src/spruce.py
@@ -1,4 +1,8 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def spruce(size):
+    print("a spruce!")
+    for i in range(size):
+        print(" " * (size - i - 1) + "*" * (2 * i + 1))
+    print(" " * (size - 1) + "*")
+
 if __name__ == "__main__":
     spruce(5)
