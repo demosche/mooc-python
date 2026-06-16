e8a204f1f9a6bd68f14191114168a144dbc7ea46
demosche <misan3760@gmail.com>
2026-06-16 17:11:29 +0500

    NOW completed greatest_number

--- mooc-programming-24/part04-09_greatest_number/src/greatest_number.py
+++ mooc-programming-24/part04-09_greatest_number/src/greatest_number.py
@@ -1,18 +1,16 @@
-def same_chars(string, i1, i2):
-    if i1 < 0 or i1 >= len(string) or i2 < 0 or i2 >= len(string):
-        return False
-    
-    return string[i1] == string[i2]
+def greatest_number(x, y, z):
+    if x >= y and x >= z:
+        return x
+    elif y >= x and y >= z:
+        return y
+    else:
+        return z
 
 
 if __name__ == "__main__":
-    print(same_chars("programmer", 6, 7))
+    greatest = greatest_number(5, 4, 8)
+    print(greatest)
     
-    print(same_chars("programmer", 0, 4))
-    
-    print(same_chars("programmer", 0, 12))
-    
-    print(same_chars("hello", 0, 0))
-    print(same_chars("hello", 0, 2))
-    print(same_chars("hello", -1, 2))
-    print(same_chars("hello", 0, 10))
+    print(greatest_number(3, 7, 2))
+    print(greatest_number(10, 10, 10))
+    print(greatest_number(-5, -2, -8))
