5421115c2ba376c01b2d9b34be397249def5e9d9
demosche <misan3760@gmail.com>
2026-06-24 21:49:07 +0500

    do first_second_last

--- mooc-programming-24/part04-11_first_second_last/src/first_second_last.py
+++ mooc-programming-24/part04-11_first_second_last/src/first_second_last.py
@@ -1,5 +1,12 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def first_word(s):
+    return s.split()[0]
+
+def second_word(s):
+    return s.split()[1]
+
+def last_word(s):
+    return s.split()[-1]
+
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
     print(first_word(sentence))
