a171615580e81b05e164a57e94827e02a2818681
demosche <misan3760@gmail.com>
2026-06-27 18:26:09 +0500

    do addItemsToList

--- mooc-programming-24/part04-13_add_items_to_list/src/add_items_to_list.py
+++ mooc-programming-24/part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,8 @@
-# Write your solution here
+count = int(input("How many items: "))
+items = []
+
+for i in range(1, count + 1):
+    value = int(input(f"Item {i}: "))
+    items.append(value)
+
+print(items)
