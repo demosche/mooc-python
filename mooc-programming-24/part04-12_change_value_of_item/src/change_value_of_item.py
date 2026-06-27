7839892f02014f92e5336a93433a8caaacfd6cf7
demosche <misan3760@gmail.com>
2026-06-27 18:22:05 +0500

    do changeValueOfItem

--- mooc-programming-24/part04-12_change_value_of_item/src/change_value_of_item.py
+++ mooc-programming-24/part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,11 @@
-# Write your solution here
+numbers = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Index: "))
+    
+    if index == -1:
+        break
+    
+    newP = int(input("New value: "))
+    numbers[index] = newP
+    print(numbers)
